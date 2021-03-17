a61b6d5870b25be3a499be35f98370072b5a271b
Hamed Moghadasi <h4lmed@gmail.com>
2021-03-17 12:52:26 +0330

    remove unused files.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-var table = $("#table").DataTable(datatableConfig.options());
+$(document).ready(function () {
+  var table = $("#table").DataTable(datatableConfig.options());
 
-addUserModule.init();
-editUserModule.init();
-delteUserModule.init();
+  addUserModule.init();
+  editUserModule.init();
+  delteUserModule.init();
+
+  $("input").attr("autocomplete", "off");
+  $("#loading").fadeOut(1000);
+});
